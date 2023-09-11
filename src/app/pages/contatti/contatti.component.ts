6aada4f33fd43d5c37399e8b55ec9c7d8a1846d5
anthony <anthony.cecconi.91@gmail.com>
2023-09-11 18:11:22 +0200

    ripsrsto bug, aggiornato form, aggiunto manuale

--- src/app/pages/contatti/contatti.component.ts
+++ src/app/pages/contatti/contatti.component.ts
@@ -7,14 +7,33 @@ import { Component } from '@angular/core';
 })
 export class ContattiComponent {
 
-  show : number = 0;
+  index : number = 1;
+  service : string = "pul"
 
   next():void{
-    this.show++
+    this.index++
   }
 
   back():void{
-    this.show--;
+    this.index--;
+  }
+
+  showControl(pos:number,type : string | null):String{
+    if (type != null) {
+      if (pos == this.index && type == this.service) {
+        return "";
+      }
+      return "d-none";
+    }
+
+    if (pos == this.index) {
+      return "";
+    }
+    return "d-none";
+  }
+
+  changeType(type :string){
+    this.service = type;
   }
 
 }
